e88e49af060421a32a2deb1f45717bc158c9c2ca
Faraaz Mohsin <49964697+faraazmohsin@users.noreply.github.com>
2023-04-07 04:04:04 -0400

    added

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-from flask import Flask, request, jsonify
+import streamlit as st
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
 import joblib
 
-app = Flask(__name__)
-model = joblib.load("knn_model.pkl")
+model = joblib.load('knn_model.pkl')
 
-@app.route('/optimize_route', methods=['POST'])
-def optimize_route():
-    data = request.get_json()
-    locations = data['locations']
-    cluster_assignments = model.predict(locations)
-    result = {'routes': cluster_assignments.tolist()}
-    return jsonify(result)
+speed_limit = st.slider('Speed Limit', 0, 100)
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+x_center = st.text_input('X Center')
+y_center = st.text_input('Y Center')
+
+class_names = ['Class 1', 'Class 2', 'Class 3']
+selected_class = st.selectbox('Select a Class', class_names)
+
+if st.button('Run Model'):
+    # Convert inputs to a DataFrame
+    input_df = pd.DataFrame({'speedLimit': speed_limit,
+                             'xCenter': x_center,
+                             'yCenter': y_center},
+                             index=[0])
+    
+    prediction = model.predict(input_df)
+    
+    st.write(f'The predicted class is: {prediction}')
