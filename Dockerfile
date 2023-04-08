3207033b0252e50da36593a0114985acf034eff9
Faraaz Mohsin <49964697+faraazmohsin@users.noreply.github.com>
2023-04-08 00:59:24 -0400

    updated file structure

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,17 @@
-FROM python:3.9-slim
+FROM python:3.8-slim
 
 WORKDIR /app
 
-COPY requirements.txt .
-
+COPY requirements.txt ./requirements.txt
 RUN pip install --no-cache-dir -r requirements.txt
+RUN apt-get update && apt-get install -y sudo
 
-COPY app.py .
-COPY knn_model.pkl .
+COPY knn_model.pkl /app/knn_model.pkl
+COPY . /app
 
-CMD ["python", "app.py"]
+ENV PORT=8080
+ENV PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
+
+EXPOSE 8080
+
+CMD streamlit run --server.port $PORT --server.enableCORS false --server.address 0.0.0.0 app.py
--- app.py
+++ app.py
@@ -1,25 +1,33 @@
+import os
 import streamlit as st
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 import joblib
+import sys
 
-model = joblib.load('knn_model.pkl')
+def main():
+    st.set_page_config(page_title="My Streamlit App", page_icon=None, layout="centered", initial_sidebar_state="auto")
+    st.write("Streamlit app is running!")
 
-speed_limit = st.slider('Speed Limit', 0, 100)
+    # Load the model
+    model = joblib.load('knn_model.pkl')
 
-x_center = st.text_input('X Center')
-y_center = st.text_input('Y Center')
+    # Define the input widgets
+    speed_limit = st.slider('Speed Limit', 0, 100)
+    x_center = st.text_input('X Center')
+    y_center = st.text_input('Y Center')
+    class_names = ['Class 1', 'Class 2', 'Class 3']
+    selected_class = st.selectbox('Select a Class', class_names)
 
-class_names = ['Class 1', 'Class 2', 'Class 3']
-selected_class = st.selectbox('Select a Class', class_names)
+    # Handle button click
+    if st.button('Run Model'):
+        # Convert inputs to a DataFrame
+        input_df = pd.DataFrame({'speedLimit': speed_limit,
+                                 'xCenter': x_center,
+                                 'yCenter': y_center},
+                                 index=[0])
+        prediction = model.predict(input_df)
+        st.write(f'The predicted class is: {prediction}')
 
-if st.button('Run Model'):
-    # Convert inputs to a DataFrame
-    input_df = pd.DataFrame({'speedLimit': speed_limit,
-                             'xCenter': x_center,
-                             'yCenter': y_center},
-                             index=[0])
-    
-    prediction = model.predict(input_df)
-    
-    st.write(f'The predicted class is: {prediction}')
+if __name__ == "__main__":
+    main()
